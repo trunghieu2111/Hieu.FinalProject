3255236aee0156fa386ed399e3f6758ed1fb7874
Hiếu Trịnh <hieutt27@hotmail.com>
2022-01-24 11:59:00 +0700

    Lỗi Authen

---
+++ src/Hieu.FinalProject.Domain/MyTenant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.TenantManagement;
+
+namespace Hieu.FinalProject
+{
+    class MyTenant : Tenant
+    {
+
+    }
+}
